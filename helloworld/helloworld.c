b4e091229561f8a6ca2bc417efc753280a76f237
tdemps <19tanner@comcast.net>
2018-01-18 15:53:35 -0600

    actual lab1

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -1,30 +1,29 @@
 #include "lcd.h"
-#include "string.h"
-
+#include "String.h"
 /// Simple 'Hello, world' program
 /**
  * This program prints "Hello, world" to the LCD screen
  * @author Chad Nelson
  * @date 06/26/2012
  */
- 
- char msg[] = "Microcontrollers are not that fun!";
- char space[] = "                   ";
- char str[100];
- 
+ char msg[] = "Microcontrollers are lots of fun!";
+ char space[] = "                  ";
+ char string[100];
 int main (void) {
 	// Initialize the the LCD.  This also clears the screen.
 	lcd_init(); 
 
+	// Clear the LCD screen and print "Hello, world" on the LCD
+	//lcd_puts("Hello, world");
+	
 	strcat(str, space);
 	strcat(str, msg);
 	int i = 0;
-	for(i = 0; i < strlen(str);i++){
+	for(i = 0; i < strlen(str); i++){
 		lcd_printf("%.20s", str+i);
-		//wait timer
+		timer_waitMillis(300);
 	}
-	lcd_printf("Hello, world");
-
+    
 	// It is recommended that you use only lcd_init(), lcd_printf(), lcd_putc, and lcd_puts from lcd.h.
     
 	return 0;

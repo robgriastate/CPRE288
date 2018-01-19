5c3e4c70681d2b16515470a4396783fcb768218d
tdemps <19tanner@comcast.net>
2018-01-19 09:15:16 -0600

    fixed

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -6,23 +6,28 @@
  * @author Chad Nelson
  * @date 06/26/2012
  */
- char msg[] = "Microcontrollers are lots of fun!";
- char space[] = "                  ";
- char string[100];
 int main (void) {
 	// Initialize the the LCD.  This also clears the screen.
 	lcd_init(); 
 
 	// Clear the LCD screen and print "Hello, world" on the LCD
 	//lcd_puts("Hello, world");
-	
-	strcat(str, space);
-	strcat(str, msg);
-	int i = 0;
-	for(i = 0; i < strlen(str); i++){
-		lcd_printf("%.20s", str+i);
-		timer_waitMillis(300);
+	while(1==1){
+	    timer_waitMillis(300);
+	    lcd_init();
+	 char msg[] = "Nooooooooooooo";
+	 char space[] = "                   ";
+	 char str[100] = "";
+	    strcat(str, space);
+	    strcat(str, msg);
+	    int i = 0;
+	    for(i = 0; i < strlen(str)+1; i++){
+	        lcd_printf("%.20s", str+i);
+	        timer_waitMillis(300);
+	    }
+
 	}
+
     
 	// It is recommended that you use only lcd_init(), lcd_printf(), lcd_putc, and lcd_puts from lcd.h.
     
